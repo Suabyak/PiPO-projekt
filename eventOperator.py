acf621e787a5be008014ec2437ff51fa71637d54
Arkadiusz Torba <suabyak@gmail.com>
2021-12-09 15:20:30 +0100

    Tworzenie okienka i operator eventów

---
+++ eventOperator.py
@@ -0,0 +1,32 @@
+from pygame import event as Event, constants
+
+
+class EventOperator:
+    """Klasa do obsługi eventów."""
+
+    def __init__(self, main):
+        """Przekazanie do obiektu odniesienia do głównej klasy."""
+        self.__main = main
+        self.__initEventOperators()
+
+    def operateEvents(self):
+        """Tutaj są wywoływane funkcje obsługujące eventy,
+        te które nie mają funkcji obsługującej
+        są wypisywane w konsoli."""
+        for event in Event.get():
+            try:
+                self.__operatableEvents[event.type](event)
+            except:
+                print(event)
+
+    def __initEventOperators(self):
+        """Tutaj dodajemy obsługę wszystkich eventów
+        które chcemy obsługiwać.
+        Działa to w ten sposób że do słownika
+        Przypisujemy jakiś typ eventu jako key
+        i jakąś funkcje jako value.
+        Funkcje obsługujące eventy muszą mieć 2 argumenty
+        pierwszy self a drugi event który jest
+        w tym momencie obsługiwany."""
+        self.__operatableEvents = dict()
+        self.__operatableEvents[constants.QUIT] = self.__main.quit
--- main.py
+++ main.py
@@ -1,6 +1,27 @@
+import pygame
+from eventOperator import EventOperator
+
+
 class Main:
     def __init__(self):
-        print("Hello World!")
-#siema
+        pygame.init()  # Inicjalizacja pygame
+        self.__eventOperator = EventOperator(self)
+        self.__display = pygame.display.set_mode((800, 640))
+        self.__FPSLimit = 144
+        self.__clock = pygame.time.Clock()
+        self.__running = True
+        self.__gameLoop()
+
+    def __gameLoop(self):
+        """Pętla która będzie się wykonywała cały czas dopóki użytkownik
+        nie zechce wyjść z aplikacji, obsługuje ona renderowanie i eventy."""
+        while self.__running:
+            self.__eventOperator.operateEvents()
+            self.__clock.tick(self.__FPSLimit)
+
+    def quit(self, event):
+        self.__running = False
+
+
 if __name__ == "__main__":
     Main()
