c2b74b43cb7f330e1db7ba8eaecda75cd06f837d
Arkadiusz Torba <suabyak@gmail.com>
2021-12-16 22:18:28 +0100

    lekka zmiana w Label

--- Renderer/Objects/label.py
+++ Renderer/Objects/label.py
@@ -3,7 +3,7 @@ from Renderer.Objects.Components.text import Text
 
 
 class Label(Object):
-    def __init__(self, id, text, size, position,
+    def __init__(self, id, text, size, position=(0, 0),
                  color=(255, 255, 255), origin=(0, 0)):
         super().__init__(id, position)
         self.addComponent(Text(text, size, color, origin))
