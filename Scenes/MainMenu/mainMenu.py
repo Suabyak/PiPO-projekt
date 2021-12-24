e7085539fb49e4e81231f2ce11fd0466d6a6a630
Arkadiusz Torba <suabyak@gmail.com>
2021-12-25 00:36:56 +0100

    Tytuł w menu głównym

--- Scenes/MainMenu/mainMenu.py
+++ Scenes/MainMenu/mainMenu.py
@@ -1,5 +1,6 @@
 from Renderer.scene import Scene
 from Renderer.Objects.button import Button
+from Renderer.Objects.colouredLabel import ColouredLabel
 from Renderer.Objects.Components.text import Text
 from functools import partial
 
@@ -9,10 +10,12 @@ class MainMenu(Scene):
 
     def __init__(self, main):
         super().__init__("MainMenu", main)
-        self.addObject(Button("Play", Text("Graj", 64, origin=(0.5, 0)),
-                              partial(MainMenu.play, self.getMain()), position=(self.getScreenSize()[0]/2, 100)))
-        self.addObject(Button("Quit", Text("Wyjdź z Gry", 64, origin=(0.5, 0)),
-                              self.getMain().quit, position=(self.getScreenSize()[0]/2, 300)))
+        self.addObject(ColouredLabel("Title", "Strażak /bBam/-/rBam/",
+                                     32, origin=(0.5, 0), position=(self.getScreenSize()[0]/2, 50)))
+        self.addObject(Button("Play", Text("Graj", 48, origin=(0.5, 0)),
+                              partial(MainMenu.play, self.getMain()), position=(self.getScreenSize()[0]/2, 200)))
+        self.addObject(Button("Quit", Text("Wyjdź z Gry", 48, origin=(0.5, 0)),
+                              self.getMain().quit, position=(self.getScreenSize()[0]/2, 400)))
 
     def play(game, event):
         game.setActiveScene("Game")
