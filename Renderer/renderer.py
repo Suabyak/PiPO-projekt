ef868363e36a272b3e3d635b55c5943f0377aea8
Arkadiusz Torba <suabyak@gmail.com>
2021-12-30 11:10:21 +0100

    dodatkowa opcja renderowania

--- Renderer/renderer.py
+++ Renderer/renderer.py
@@ -15,14 +15,18 @@ class Renderer:
         self.__display.fill((0, 0, 0))
         for obj in scene.getObjects():
             if (obj.isActive() and obj.isRenderable()):
-                position = obj.getComponent("Transform").getPosition()
-                for componentType, component in obj.getComponents().items():
-                    if not component.isRenderable():
-                        continue
-                    surface, origin = component.render()
-                    surface.set_alpha(obj.getVisibility())
-                    surfaceSize = surface.get_size()
-                    renderPosition = (position[0]-surfaceSize[0]*(origin.x),
-                                      position[1]-surfaceSize[1]*(origin.y))
-                    self.__display.blit(surface, renderPosition)
+                try:
+                    surface, position = obj.render()
+                    self.__display.blit(surface, position)
+                except Exception:
+                    position = obj.getComponent("Transform").getPosition()
+                    for componentType, component in obj.getComponents().items():
+                        if not component.isRenderable():
+                            continue
+                        surface, origin = component.render()
+                        surface.set_alpha(obj.getVisibility())
+                        surfaceSize = surface.get_size()
+                        renderPosition = (position[0]-surfaceSize[0]*(origin.x),
+                                          position[1]-surfaceSize[1]*(origin.y))
+                        self.__display.blit(surface, renderPosition)
         Display.flip()
